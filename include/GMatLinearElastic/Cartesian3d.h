b2aaaf5636770864b3449ad7673631bf67ce9dee
Tom de Geus <tom@geus.me>
2018-11-08 09:48:00 +0100

    Simplified structed, only 3d

--- include/GMatLinearElastic/Cartesian3d.h
+++ include/GMatLinearElastic/Cartesian3d.h
@@ -14,17 +14,18 @@
 // =================================================================================================
 
 namespace GMatLinearElastic {
+namespace Cartesian3d {
 
 // -------------------------------------------------------------------------------------------------
 
-class Cartesian3d
+class Material
 {
 public:
 
   // constructors
-  Cartesian3d() = default;
-  Cartesian3d(size_t nelem, size_t nip);
-  Cartesian3d(size_t nelem, size_t nip, double kappa, double mu);
+  Material() = default;
+  Material(size_t nelem, size_t nip);
+  Material(size_t nelem, size_t nip, double kappa, double mu);
 
   // return shape
   size_t nelem() const;
@@ -41,16 +42,12 @@ public:
   void set(const xt::xtensor<size_t,2> &I, double kappa, double mu);
 
   // compute (no allocation)
-  void Sig      (const xt::xtensor<double,4> &a_Eps  , xt::xtensor<double,4> &a_Sig      ) const;
-  void Sig2d    (const xt::xtensor<double,4> &a_Eps2d, xt::xtensor<double,4> &a_Sig2d    ) const;
-  void Tangent  (                                      xt::xtensor<double,6> &a_Tangent  ) const;
-  void Tangent2d(                                      xt::xtensor<double,6> &a_Tangent2d) const;
+  void Sig    (const xt::xtensor<double,4> &a_Eps, xt::xtensor<double,4> &a_Sig    ) const;
+  void Tangent(                                    xt::xtensor<double,6> &a_Tangent) const;
 
   // compute (return allocated result)
-  xt::xtensor<double,4> Sig      (const xt::xtensor<double,4> &a_Eps  ) const;
-  xt::xtensor<double,4> Sig2d    (const xt::xtensor<double,4> &a_Eps2d) const;
-  xt::xtensor<double,6> Tangent  (                                    ) const;
-  xt::xtensor<double,6> Tangent2d(                                    ) const;
+  xt::xtensor<double,4> Sig    (const xt::xtensor<double,4> &a_Eps) const;
+  xt::xtensor<double,6> Tangent(                                  ) const;
 
 private:
 
@@ -59,32 +56,10 @@ private:
   xt::xtensor<double,2> m_mu;
   size_t                m_nelem;
   size_t                m_nip;
+  static const size_t   m_ndim=3;
 };
 
 // -------------------------------------------------------------------------------------------------
-
-namespace Support {
-namespace Cartesian2d {
-
-using T2 = xt::xtensor_fixed<double, xt::xshape<2,2>>;
-using T4 = xt::xtensor_fixed<double, xt::xshape<2,2,2,2>>;
-
-template<class T> double trace (const T &A);
-template<class T> double ddot22(const T &A, const T &B);
-
-T2 I();
-T4 II();
-T4 I4();
-T4 I4rt();
-T4 I4s();
-T4 I4d();
-
-}}
-
-// -------------------------------------------------------------------------------------------------
-
-namespace Support {
-namespace Cartesian3d {
 
 using T2 = xt::xtensor_fixed<double, xt::xshape<3,3>>;
 using T4 = xt::xtensor_fixed<double, xt::xshape<3,3,3,3>>;
@@ -99,11 +74,9 @@ T4 I4rt();
 T4 I4s();
 T4 I4d();
 
-}}
-
 // -------------------------------------------------------------------------------------------------
 
-} // namespace ...
+}} // namespace ...
 
 // -------------------------------------------------------------------------------------------------
 
--- include/GMatLinearElastic/Cartesian3d.hpp
+++ include/GMatLinearElastic/Cartesian3d.hpp
@@ -14,112 +14,6 @@
 // =================================================================================================
 
 namespace GMatLinearElastic {
-
-// =================================================================================================
-
-namespace Support {
-namespace Cartesian2d {
-
-// -------------------------------------------------------------------------------------------------
-
-template<class T>
-inline double trace(const T &A)
-{
-  return A(0,0) + A(1,1);
-}
-
-// -------------------------------------------------------------------------------------------------
-
-template<class T>
-inline double ddot22(const T &A, const T &B)
-{
-  return A(0,0) * B(0,0) + 2.0 * A(0,1) * B(0,1) +
-         A(1,1) * B(1,1);
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T2 I()
-{
-  return T2({{1., 0.},
-             {0., 1.}});
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T4 II()
-{
-  T4 out;
-
-  out.fill(0.0);
-
-  for ( size_t i = 0 ; i < 2 ; ++i )
-    for ( size_t j = 0 ; j < 2 ; ++j )
-      for ( size_t k = 0 ; k < 2 ; ++k )
-        for ( size_t l = 0 ; l < 2 ; ++l )
-          if ( i == j and k == l )
-            out(i,j,k,l) = 1.;
-
-  return out;
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T4 I4()
-{
-  T4 out;
-
-  out.fill(0.0);
-
-  for ( size_t i = 0 ; i < 2 ; ++i )
-    for ( size_t j = 0 ; j < 2 ; ++j )
-      for ( size_t k = 0 ; k < 2 ; ++k )
-        for ( size_t l = 0 ; l < 2 ; ++l )
-          if ( i == l and j == k )
-            out(i,j,k,l) = 1.;
-
-  return out;
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T4 I4rt()
-{
-  T4 out;
-
-  out.fill(0.0);
-
-  for ( size_t i = 0 ; i < 2 ; ++i )
-    for ( size_t j = 0 ; j < 2 ; ++j )
-      for ( size_t k = 0 ; k < 2 ; ++k )
-        for ( size_t l = 0 ; l < 2 ; ++l )
-          if ( i == k and j == l )
-            out(i,j,k,l) = 1.;
-
-  return out;
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T4 I4s()
-{
-  return .5 * ( I4() + I4rt() );
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline T4 I4d()
-{
-  return I4s() - II()/3.;
-}
-
-// -------------------------------------------------------------------------------------------------
-
-}}
-
-// =================================================================================================
-
-namespace Support {
 namespace Cartesian3d {
 
 // -------------------------------------------------------------------------------------------------
@@ -219,11 +113,7 @@ inline T4 I4d()
 
 // -------------------------------------------------------------------------------------------------
 
-}}
-
-// =================================================================================================
-
-inline Cartesian3d::Cartesian3d(size_t nelem, size_t nip) : m_nelem(nelem), m_nip(nip)
+inline Material::Material(size_t nelem, size_t nip) : m_nelem(nelem), m_nip(nip)
 {
   m_set   = xt::zeros<int   >({nelem, nip});
   m_kappa = xt::empty<double>({nelem, nip});
@@ -232,7 +122,7 @@ inline Cartesian3d::Cartesian3d(size_t nelem, size_t nip) : m_nelem(nelem), m_ni
 
 // -------------------------------------------------------------------------------------------------
 
-inline Cartesian3d::Cartesian3d(size_t nelem, size_t nip, double kappa, double mu) :
+inline Material::Material(size_t nelem, size_t nip, double kappa, double mu) :
   m_nelem(nelem), m_nip(nip)
 {
   m_set   = xt::ones<int   >({nelem, nip});
@@ -242,35 +132,35 @@ inline Cartesian3d::Cartesian3d(size_t nelem, size_t nip, double kappa, double m
 
 // -------------------------------------------------------------------------------------------------
 
-inline size_t Cartesian3d::nelem() const
+inline size_t Material::nelem() const
 {
   return m_nelem;
 }
 
 // -------------------------------------------------------------------------------------------------
 
-inline size_t Cartesian3d::nip() const
+inline size_t Material::nip() const
 {
   return m_nip;
 }
 
 // -------------------------------------------------------------------------------------------------
 
-inline xt::xtensor<double,2> Cartesian3d::kappa() const
+inline xt::xtensor<double,2> Material::kappa() const
 {
   return m_kappa;
 }
 
 // -------------------------------------------------------------------------------------------------
 
-inline xt::xtensor<double,2> Cartesian3d::mu() const
+inline xt::xtensor<double,2> Material::mu() const
 {
   return m_mu;
 }
 
 // -------------------------------------------------------------------------------------------------
 
-inline void Cartesian3d::check() const
+inline void Material::check() const
 {
   for ( size_t e = 0 ; e < m_nelem ; ++e )
     for ( size_t q = 0 ; q < m_nip ; ++q )
@@ -280,7 +170,7 @@ inline void Cartesian3d::check() const
 
 // -------------------------------------------------------------------------------------------------
 
-inline void Cartesian3d::set(const xt::xtensor<size_t,2> &I, double kappa, double mu)
+inline void Material::set(const xt::xtensor<size_t,2> &I, double kappa, double mu)
 {
   // check input
   #ifndef NDEBUG
@@ -307,20 +197,20 @@ inline void Cartesian3d::set(const xt::xtensor<size_t,2> &I, double kappa, doubl
 
 // -------------------------------------------------------------------------------------------------
 
-inline void Cartesian3d::Sig(const xt::xtensor<double,4> &a_Eps, xt::xtensor<double,4> &a_Sig) const
+inline void Material::Sig(const xt::xtensor<double,4> &a_Eps, xt::xtensor<double,4> &a_Sig) const
 {
   // check input
   assert( a_Eps.shape()[0] == m_nelem       );
   assert( a_Eps.shape()[1] == m_nip         );
-  assert( a_Eps.shape()[2] == 3             );
-  assert( a_Eps.shape()[3] == 3             );
+  assert( a_Eps.shape()[2] == m_ndim        );
+  assert( a_Eps.shape()[3] == m_ndim        );
   assert( a_Eps.shape()    == a_Sig.shape() );
 
   // start threads (all allocated variables inside this block are local to each thread)
   #pragma omp parallel
   {
     // identity tensor
-    Support::Cartesian3d::T2 I = Support::Cartesian3d::I();
+    T2 I = Cartesian3d::I();
 
     // loop over all points
     #pragma omp for
@@ -329,13 +219,13 @@ inline void Cartesian3d::Sig(const xt::xtensor<double,4> &a_Eps, xt::xtensor<dou
       for ( size_t q = 0 ; q < m_nip ; ++q )
       {
         // - alias
-        auto  Eps   = xt::adapt(&a_Eps(e,q,0,0), xt::xshape<3,3>());
+        auto  Eps   = xt::adapt(&a_Eps(e,q,0,0), xt::xshape<m_ndim,m_ndim>());
         auto  Sig   = xt::view(a_Sig,e,q);
         auto& kappa = m_kappa(e,q);
         auto& mu    = m_mu   (e,q);
         // - compute strain components
-        auto treps = Support::Cartesian3d::trace(Eps);
-        auto Epsd  = Eps - treps/3. * I;
+        auto  treps = trace(Eps);
+        auto  Epsd  = Eps - treps/3. * I;
         // - compute stress tensor
         Sig = kappa * treps * I + 2. * mu * Epsd;
       }
@@ -345,7 +235,43 @@ inline void Cartesian3d::Sig(const xt::xtensor<double,4> &a_Eps, xt::xtensor<dou
 
 // -------------------------------------------------------------------------------------------------
 
-inline xt::xtensor<double,4> Cartesian3d::Sig(const xt::xtensor<double,4> &a_Eps) const
+inline void Material::Tangent(xt::xtensor<double,6> &a_Tangent) const
+{
+  // check input
+  assert( a_Tangent.shape()[0] == m_nelem );
+  assert( a_Tangent.shape()[1] == m_nip   );
+  assert( a_Tangent.shape()[2] == m_ndim  );
+  assert( a_Tangent.shape()[3] == m_ndim  );
+  assert( a_Tangent.shape()[4] == m_ndim  );
+  assert( a_Tangent.shape()[5] == m_ndim  );
+
+  // start threads (all allocated variables inside this block are local to each thread)
+  #pragma omp parallel
+  {
+    // identity tensor
+    T4 II  = Cartesian3d::II();
+    T4 I4d = Cartesian3d::I4d();
+
+    // loop over all points
+    #pragma omp for
+    for ( size_t e = 0 ; e < m_nelem ; ++e )
+    {
+      for ( size_t q = 0 ; q < m_nip ; ++q )
+      {
+        // - alias
+        auto  C4    = xt::view(a_Tangent,e,q);
+        auto& kappa = m_kappa(e,q);
+        auto& mu    = m_mu   (e,q);
+        // - compute
+        C4 = kappa * II + 2. * mu * I4d;
+      }
+    }
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+inline xt::xtensor<double,4> Material::Sig(const xt::xtensor<double,4> &a_Eps) const
 {
   xt::xtensor<double,4> a_Sig = xt::empty<double>(a_Eps.shape());
 
@@ -356,154 +282,18 @@ inline xt::xtensor<double,4> Cartesian3d::Sig(const xt::xtensor<double,4> &a_Eps
 
 // -------------------------------------------------------------------------------------------------
 
-inline void Cartesian3d::Sig2d(const xt::xtensor<double,4> &a_Eps, xt::xtensor<double,4> &a_Sig) const
+inline xt::xtensor<double,6> Material::Tangent() const
 {
-  // check input
-  assert( a_Eps.shape()[0] == m_nelem       );
-  assert( a_Eps.shape()[1] == m_nip         );
-  assert( a_Eps.shape()[2] == 2             );
-  assert( a_Eps.shape()[3] == 2             );
-  assert( a_Eps.shape()    == a_Sig.shape() );
-
-  // start threads (all allocated variables inside this block are local to each thread)
-  #pragma omp parallel
-  {
-    // identity tensor
-    Support::Cartesian2d::T2 I = Support::Cartesian2d::I();
-
-    // loop over all points
-    #pragma omp for
-    for ( size_t e = 0 ; e < m_nelem ; ++e )
-    {
-      for ( size_t q = 0 ; q < m_nip ; ++q )
-      {
-        // - alias
-        auto  Eps   = xt::adapt(&a_Eps(e,q,0,0), xt::xshape<2,2>());
-        auto  Sig   = xt::view(a_Sig,e,q);
-        auto& kappa = m_kappa(e,q);
-        auto& mu    = m_mu   (e,q);
-        // - compute strain components
-        auto treps = Support::Cartesian2d::trace(Eps);
-        auto Epsd  = Eps - treps/3. * I;
-        // - compute stress tensor
-        Sig = kappa * treps * I + 2. * mu * Epsd;
-      }
-    }
-  }
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline xt::xtensor<double,4> Cartesian3d::Sig2d(const xt::xtensor<double,4> &a_Eps) const
-{
-  xt::xtensor<double,4> a_Sig = xt::empty<double>(a_Eps.shape());
-
-  this->Sig2d(a_Eps, a_Sig);
-
-  return a_Sig;
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline void Cartesian3d::Tangent(xt::xtensor<double,6> &a_Tangent) const
-{
-  // check input
-  assert( a_Tangent.shape()[0] == m_nelem );
-  assert( a_Tangent.shape()[1] == m_nip   );
-  assert( a_Tangent.shape()[2] == 3       );
-  assert( a_Tangent.shape()[3] == 3       );
-  assert( a_Tangent.shape()[4] == 3       );
-  assert( a_Tangent.shape()[5] == 3       );
-
-  // start threads (all allocated variables inside this block are local to each thread)
-  #pragma omp parallel
-  {
-    // identity tensor
-    Support::Cartesian3d::T4 II  = Support::Cartesian3d::II();
-    Support::Cartesian3d::T4 I4d = Support::Cartesian3d::I4d();
-
-    // loop over all points
-    #pragma omp for
-    for ( size_t e = 0 ; e < m_nelem ; ++e )
-    {
-      for ( size_t q = 0 ; q < m_nip ; ++q )
-      {
-        // - alias
-        auto  C4    = xt::view(a_Tangent,e,q);
-        auto& kappa = m_kappa(e,q);
-        auto& mu    = m_mu   (e,q);
-        // - compute
-        C4 = kappa * II + 2. * mu * I4d;
-      }
-    }
-  }
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline xt::xtensor<double,6> Cartesian3d::Tangent() const
-{
-  size_t nd = 3;
-
-  xt::xtensor<double,6> a_Tangent = xt::empty<double>({m_nelem,m_nip,nd,nd,nd,nd});
+  xt::xtensor<double,6> a_Tangent = xt::empty<double>({m_nelem,m_nip,m_ndim,m_ndim,m_ndim,m_ndim});
 
   this->Tangent(a_Tangent);
 
   return a_Tangent;
 }
 
-// -------------------------------------------------------------------------------------------------
-
-inline void Cartesian3d::Tangent2d(xt::xtensor<double,6> &a_Tangent) const
-{
-  // check input
-  assert( a_Tangent.shape()[0] == m_nelem );
-  assert( a_Tangent.shape()[1] == m_nip   );
-  assert( a_Tangent.shape()[2] == 2       );
-  assert( a_Tangent.shape()[3] == 2       );
-  assert( a_Tangent.shape()[4] == 2       );
-  assert( a_Tangent.shape()[5] == 2       );
-
-  // start threads (all allocated variables inside this block are local to each thread)
-  #pragma omp parallel
-  {
-    // identity tensor
-    Support::Cartesian2d::T4 II  = Support::Cartesian2d::II();
-    Support::Cartesian2d::T4 I4d = Support::Cartesian2d::I4d();
-
-    // loop over all points
-    #pragma omp for
-    for ( size_t e = 0 ; e < m_nelem ; ++e )
-    {
-      for ( size_t q = 0 ; q < m_nip ; ++q )
-      {
-        // - alias
-        auto  C4    = xt::view(a_Tangent,e,q);
-        auto& kappa = m_kappa(e,q);
-        auto& mu    = m_mu   (e,q);
-        // - compute
-        C4 = kappa * II + 2. * mu * I4d;
-      }
-    }
-  }
-}
-
-// -------------------------------------------------------------------------------------------------
-
-inline xt::xtensor<double,6> Cartesian3d::Tangent2d() const
-{
-  size_t nd = 2;
-
-  xt::xtensor<double,6> a_Tangent = xt::empty<double>({m_nelem,m_nip,nd,nd,nd,nd});
-
-  this->Tangent2d(a_Tangent);
-
-  return a_Tangent;
-}
-
 // =================================================================================================
 
-} // namespace ...
+}} // namespace ...
 
 // =================================================================================================
 
