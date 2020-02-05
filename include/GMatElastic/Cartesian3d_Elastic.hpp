e592f6e7be520b3017d077b3f72346c91bfde569
Tom de Geus <tom@geus.me>
2020-02-05 16:56:14 +0100

    Reformatting style

--- include/GMatElastic/Cartesian3d_Elastic.hpp
+++ include/GMatElastic/Cartesian3d_Elastic.hpp
@@ -1,8 +1,8 @@
-/* =================================================================================================
+/*
 
 (c - MIT) T.W.J. de Geus (Tom) | www.geus.me | github.com/tdegeus/GMatElastic
 
-================================================================================================= */
+*/
 
 #ifndef GMATELASTIC_CARTESIAN3D_ELASTIC_HPP
 #define GMATELASTIC_CARTESIAN3D_ELASTIC_HPP
@@ -12,72 +12,57 @@
 namespace GMatElastic {
 namespace Cartesian3d {
 
-// -------------------------------------------------------------------------------------------------
-
 inline Elastic::Elastic(double K, double G) : m_K(K), m_G(G)
 {
 }
 
-// -------------------------------------------------------------------------------------------------
-
 inline double Elastic::K() const
 {
-  return m_K;
+    return m_K;
 }
-
-// -------------------------------------------------------------------------------------------------
 
 inline double Elastic::G() const
 {
-  return m_G;
+    return m_G;
 }
-
-// -------------------------------------------------------------------------------------------------
 
 template <class T>
 inline void Elastic::stress(const Tensor2& Eps, T&& Sig) const
 {
-  auto I = Cartesian3d::I2();
-  auto epsm = trace(Eps) / 3.0;
-  auto Epsd = Eps - epsm * I;
-  xt::noalias(Sig) = 3.0 * m_K * epsm * I + 2.0 * m_G * Epsd;
+    auto I = Cartesian3d::I2();
+    auto epsm = trace(Eps) / 3.0;
+    auto Epsd = Eps - epsm * I;
+    xt::noalias(Sig) = 3.0 * m_K * epsm * I + 2.0 * m_G * Epsd;
 }
-
-// -------------------------------------------------------------------------------------------------
 
 inline Tensor2 Elastic::Stress(const Tensor2& Eps) const
 {
-  Tensor2 Sig;
-  this->stress(Eps, Sig);
-  return Sig;
+    Tensor2 Sig;
+    this->stress(Eps, Sig);
+    return Sig;
 }
-
-// -------------------------------------------------------------------------------------------------
 
 template <class T, class S>
 inline void Elastic::tangent(const Tensor2& Eps, T&& Sig, S&& C) const
 {
-  auto I = Cartesian3d::I2();
-  auto II = Cartesian3d::II();
-  auto I4d = Cartesian3d::I4d();
-  auto epsm = trace(Eps) / 3.0;
-  auto Epsd = Eps - epsm * I;
-  xt::noalias(Sig) = 3.0 * m_K * epsm * I + 2.0 * m_G * Epsd;
-  xt::noalias(C) = m_K * II + 2.0 * m_G * I4d;
+    auto I = Cartesian3d::I2();
+    auto II = Cartesian3d::II();
+    auto I4d = Cartesian3d::I4d();
+    auto epsm = trace(Eps) / 3.0;
+    auto Epsd = Eps - epsm * I;
+    xt::noalias(Sig) = 3.0 * m_K * epsm * I + 2.0 * m_G * Epsd;
+    xt::noalias(C) = m_K * II + 2.0 * m_G * I4d;
 }
 
-// -------------------------------------------------------------------------------------------------
-
-inline std::tuple<Tensor2,Tensor4> Elastic::Tangent(const Tensor2& Eps) const
+inline std::tuple<Tensor2, Tensor4> Elastic::Tangent(const Tensor2& Eps) const
 {
-  Tensor2 Sig;
-  Tensor4 C;
-  this->tangent(Eps, Sig, C);
-  return std::make_tuple(Sig, C);
+    Tensor2 Sig;
+    Tensor4 C;
+    this->tangent(Eps, Sig, C);
+    return std::make_tuple(Sig, C);
 }
 
-// -------------------------------------------------------------------------------------------------
-
-}} // namespace ...
+} // namespace Cartesian3d
+} // namespace GMatElastic
 
 #endif
--- test/Cartesian3d.cpp
+++ test/Cartesian3d.cpp
@@ -1,111 +1,86 @@
 
 #include <catch2/catch.hpp>
 
-#define EQ(a,b) REQUIRE_THAT( (a), Catch::WithinAbs((b), 1.e-12) );
+#define EQ(a,b) REQUIRE_THAT((a), Catch::WithinAbs((b), 1.e-12));
 
 #include <GMatElastic/Cartesian3d.h>
 
 namespace GM = GMatElastic::Cartesian3d;
 
-// =================================================================================================
 
 TEST_CASE("GMatElastic::Cartesian3d", "Cartesian3d.h")
 {
 
-// =================================================================================================
+double K = 12.3;
+double G = 45.6;
 
-SECTION("Elastic")
+GM::Tensor2 Eps;
+GM::Tensor2 Sig;
+
+double gamma = 0.02;
+double epsm = 0.12;
+
+Eps.fill(0.0);
+Eps(0,0) = Eps(1,1) = Eps(2,2) = epsm;
+Eps(0,1) = Eps(1,0) = gamma;
+
+
+SECTION("Elastic: simple shear + volumetric deformation")
 {
-  // material model
-  // - parameters
-  double K = 12.3;
-  double G = 45.6;
-  // - model
-  GM::Elastic mat(K, G);
+    GM::Elastic mat(K, G);
 
-  // allocate tensors
-  GM::Tensor2 Eps, Sig;
+    Sig = mat.Stress(Eps);
 
-  // simple shear + volumetric deformation
-  // - parameters
-  double gamma = 0.02;
-  double epsm = 0.12;
-  // - strain
-  Eps.fill(0.0);
-  Eps(0,0) = Eps(1,1) = Eps(2,2) = epsm;
-  Eps(0,1) = Eps(1,0) = gamma;
-  // - stress
-  Sig = mat.Stress(Eps);
-  // - analytical solution
-  EQ(Sig(0,0), 3.0 * K * epsm);
-  EQ(Sig(1,1), 3.0 * K * epsm);
-  EQ(Sig(2,2), 3.0 * K * epsm);
-  EQ(Sig(0,1), 2.0 * G * gamma);
-  EQ(Sig(1,0), 2.0 * G * gamma);
-  EQ(Sig(0,2), 0);
-  EQ(Sig(1,2), 0);
-  EQ(Sig(2,0), 0);
-  EQ(Sig(2,1), 0);
+    EQ(Sig(0,0), 3.0 * K * epsm);
+    EQ(Sig(1,1), 3.0 * K * epsm);
+    EQ(Sig(2,2), 3.0 * K * epsm);
+    EQ(Sig(0,1), 2.0 * G * gamma);
+    EQ(Sig(1,0), 2.0 * G * gamma);
+    EQ(Sig(0,2), 0.0);
+    EQ(Sig(1,2), 0.0);
+    EQ(Sig(2,0), 0.0);
+    EQ(Sig(2,1), 0.0);
 }
 
-// =================================================================================================
 
-SECTION("Matrix")
+SECTION("Matrix: simple shear + volumetric deformation")
 {
-  // parameters
-  double K = 12.3;
-  double G = 45.6;
-  size_t nelem = 3;
-  size_t nip = 2;
-  size_t d = 3;
+    size_t nelem = 3;
+    size_t nip = 2;
 
-  // allocate matrix
-  GM::Matrix mat({nelem, nip});
+    GM::Matrix mat(nelem, nip);
 
-  // all rows elastic
-  {
-    xt::xtensor<size_t,2> I = xt::ones<size_t>({nelem,nip});
-    mat.setElastic(I, K, G);
-  }
+    // all rows elastic
+    {
+        xt::xtensor<size_t,2> I = xt::ones<size_t>({nelem, nip});
+        mat.setElastic(I, K, G);
+    }
 
-  // allocate tensors
-  GM::Tensor2 Eps;
+    xt::xtensor<double,4> eps = xt::empty<double>({nelem, nip, 3ul, 3ul});
 
-  // simple shear + volumetric deformation
-  // - parameters
-  double gamma = 0.02;
-  double epsm = 0.12;
-  // - strain
-  Eps.fill(0.0);
-  Eps(0,0) = Eps(1,1) = Eps(2,2) = epsm;
-  Eps(0,1) = Eps(1,0) = gamma;
-  // - strain/stress matrices
-  xt::xtensor<double,4> eps = xt::empty<double>({nelem, nip, d, d});
-  xt::xtensor<double,4> sig;
-  // - set strain
-  for (size_t e = 0; e < nelem; ++e)
-    for (size_t q = 0; q < nip; ++q)
-      xt::view(eps, e, q) = Eps;
-  // - stress
-  sig = mat.Stress(eps);
+    for (size_t e = 0; e < nelem; ++e) {
+        for (size_t q = 0; q < nip; ++q) {
+            xt::view(eps, e, q) = Eps;
+        }
+    }
 
-  // - analytical solution
-  EQ(sig(0,0,0,0), 3.0 * K * epsm);  EQ(sig(0,1,0,0), 3.0 * K * epsm);
-  EQ(sig(0,0,1,1), 3.0 * K * epsm);  EQ(sig(0,1,1,1), 3.0 * K * epsm);
-  EQ(sig(0,0,2,2), 3.0 * K * epsm);  EQ(sig(0,1,2,2), 3.0 * K * epsm);
-  EQ(sig(0,0,0,1), 2.0 * G * gamma); EQ(sig(0,1,0,1), 2.0 * G * gamma);
-  EQ(sig(0,0,1,0), 2.0 * G * gamma); EQ(sig(0,1,1,0), 2.0 * G * gamma);
-  EQ(sig(1,0,0,0), 3.0 * K * epsm);  EQ(sig(1,1,0,0), 3.0 * K * epsm);
-  EQ(sig(1,0,1,1), 3.0 * K * epsm);  EQ(sig(1,1,1,1), 3.0 * K * epsm);
-  EQ(sig(1,0,2,2), 3.0 * K * epsm);  EQ(sig(1,1,2,2), 3.0 * K * epsm);
-  EQ(sig(1,0,0,1), 2.0 * G * gamma); EQ(sig(1,1,0,1), 2.0 * G * gamma);
-  EQ(sig(1,0,1,0), 2.0 * G * gamma); EQ(sig(1,1,1,0), 2.0 * G * gamma);
-  REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(0), xt::keep(2)), 0));
-  REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(1), xt::keep(2)), 0));
-  REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(2), xt::keep(0)), 0));
-  REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(2), xt::keep(1)), 0));
+    auto sig = mat.Stress(eps);
+
+    for (size_t e = 0; e < nelem; ++e) {
+        for (size_t q = 0; q < nip; ++q) {
+            EQ(sig(e,q,0,0), 3.0 * K * epsm);
+            EQ(sig(e,q,1,1), 3.0 * K * epsm);
+            EQ(sig(e,q,2,2), 3.0 * K * epsm);
+            EQ(sig(e,q,0,1), 2.0 * G * gamma);
+            EQ(sig(e,q,1,0), 2.0 * G * gamma);
+        }
+    }
+
+    REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(0), xt::keep(2)), 0.0));
+    REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(1), xt::keep(2)), 0.0));
+    REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(2), xt::keep(0)), 0.0));
+    REQUIRE(xt::allclose(xt::view(sig, xt::all(), xt::all(), xt::keep(2), xt::keep(1)), 0.0));
 }
 
-// =================================================================================================
 
 }
