fe795e2ab14bd8f0dced761f57ecee9ffabc563e
Tom de Geus <tdegeus@users.noreply.github.com>
2020-02-06 09:48:19 +0100

    Reformatting style (#9)

--- include/GMatElastic/config.h
+++ include/GMatElastic/config.h
@@ -1,13 +1,11 @@
-/* =================================================================================================
+/*
 
 (c - MIT) T.W.J. de Geus (Tom) | tom@geus.me | www.geus.me | github.com/tdegeus/GMatElastic
 
-================================================================================================= */
+*/
 
 #ifndef GMATELASTIC_H
 #define GMATELASTIC_H
-
-// -------------------------------------------------------------------------------------------------
 
 #include <tuple>
 #include <stdexcept>
@@ -27,39 +25,34 @@
 #include <xtensor/xsort.hpp>
 #include <xtensor/xmath.hpp>
 
-// -------------------------------------------------------------------------------------------------
-
-#ifndef NDEBUG
-#define GMATELASTIC_ENABLE_ASSERT
-#endif
-
 #ifdef GMATELASTIC_ENABLE_ASSERT
-#define GMATELASTIC_ASSERT(expr) GMATELASTIC_ASSERT_IMPL(expr, __FILE__, __LINE__)
-#define GMATELASTIC_ASSERT_IMPL(expr, file, line)                                                                         \
-    if (!(expr))                                                                                                          \
-    {                                                                                                                     \
-        throw std::runtime_error(std::string(file) + ':' + std::to_string(line) + ": assertion failed (" #expr ") \n\t"); \
-    }
-#else
-#define GMATELASTIC_ASSERT(expr)
-#endif
 
-// -------------------------------------------------------------------------------------------------
+    #define GMATELASTIC_ASSERT(expr) GMATELASTIC_ASSERT_IMPL(expr, __FILE__, __LINE__)
+    #define GMATELASTIC_ASSERT_IMPL(expr, file, line) \
+        if (!(expr)) { \
+            throw std::runtime_error( \
+                std::string(file) + ':' + std::to_string(line) + \
+                ": assertion failed (" #expr ") \n\t"); \
+        }
+
+#else
+
+    #define GMATELASTIC_ASSERT(expr)
+
+#endif
 
 #define GMATELASTIC_VERSION_MAJOR 0
-#define GMATELASTIC_VERSION_MINOR 0
-#define GMATELASTIC_VERSION_PATCH 4
+#define GMATELASTIC_VERSION_MINOR 1
+#define GMATELASTIC_VERSION_PATCH 0
 
-#define GMATELASTIC_VERSION_AT_LEAST(x,y,z) \
-  (GMATELASTIC_VERSION_MAJOR > x || (GMATELASTI_VERSION_MAJOR >= x && \
-  (GMATELASTIC_VERSION_MINOR > y || (GMATELASTI_VERSION_MINOR >= y && \
-                                     GMATELASTI_VERSION_PATCH >= z))))
+#define GMATELASTIC_VERSION_AT_LEAST(x, y, z) \
+    (GMATELASTIC_VERSION_MAJOR > x || (GMATELASTI_VERSION_MAJOR >= x && \
+    (GMATELASTIC_VERSION_MINOR > y || (GMATELASTI_VERSION_MINOR >= y && \
+                                       GMATELASTI_VERSION_PATCH >= z))))
 
-#define GMATELASTIC_VERSION(x,y,z) \
-  (GMATELASTIC_VERSION_MAJOR == x && \
-   GMATELASTIC_VERSION_MINOR == y && \
-   GMATELASTIC_VERSION_PATCH == z)
-
-// -------------------------------------------------------------------------------------------------
+#define GMATELASTIC_VERSION(x, y, z) \
+    (GMATELASTIC_VERSION_MAJOR == x && \
+     GMATELASTIC_VERSION_MINOR == y && \
+     GMATELASTIC_VERSION_PATCH == z)
 
 #endif
--- python/main.cpp
+++ python/main.cpp
@@ -1,8 +1,8 @@
-/* =================================================================================================
+/*
 
-(c - MIT) T.W.J. de Geus (Tom) | tom@geus.me | www.geus.me | github.com/tdegeus/GMatElastic
+(c - MIT) T.W.J. de Geus (Tom) | www.geus.me | github.com/tdegeus/GMatElastic
 
-================================================================================================= */
+*/
 
 #include <pybind11/pybind11.h>
 #include <pyxtensor/pyxtensor.hpp>
@@ -11,119 +11,122 @@
 // (doesn't cost a lot of time, but avoids segmentation faults)
 #define GMATELASTIC_ENABLE_ASSERT
 
-// include library
 #include <GMatElastic/Cartesian3d.h>
 
-// abbreviate name-space
 namespace py = pybind11;
 
-// -------------------------------------------------------------------------------------------------
-
-PYBIND11_MODULE(GMatElastic, m) {
+PYBIND11_MODULE(GMatElastic, m)
+{
 
 m.doc() = "Linear elastic material model";
 
-// create submodule
+// -----------------------
+// GMatElastic.Cartesian3d
+// -----------------------
+
 py::module sm = m.def_submodule("Cartesian3d", "3d Cartesian coordinates");
 
-// abbreviate name-space
 namespace SM = GMatElastic::Cartesian3d;
 
-// -------------------------------------------------------------------------------------------------
+// Unit tensors
 
-sm.def("I2", &SM::I2);
-sm.def("II", &SM::II);
-sm.def("I4", &SM::I4);
-sm.def("I4rt", &SM::I4rt);
-sm.def("I4s", &SM::I4s);
-sm.def("I4d", &SM::I4d);
+sm.def("I2", &SM::I2, "Second order unit tensor.");
 
-// -------------------------------------------------------------------------------------------------
+sm.def("II", &SM::II, "Fourth order tensor with the result of the dyadic product II.");
 
-sm.def("Hydrostatic",
-  py::overload_cast<const SM::Tensor2&>(&SM::Hydrostatic),
-  "Hydrostatic part of a 2nd-order tensor",
-  py::arg("A"));
+sm.def("I4", &SM::I4, "Fourth order unit tensor.");
 
-sm.def("Deviatoric",
-  py::overload_cast<const SM::Tensor2&>(&SM::Deviatoric),
-  "Deviatoric",
-  py::arg("A"));
+sm.def("I4rt", &SM::I4rt, "Fourth right-transposed order unit tensor.");
 
-sm.def("Epseq",
-  py::overload_cast<const SM::Tensor2&>(&SM::Epseq),
-  "Equivalent strain deviator",
-  py::arg("Eps"));
+sm.def("I4s", &SM::I4s, "Fourth order symmetric projection tensor.");
 
-sm.def("Sigeq",
-  py::overload_cast<const SM::Tensor2&>(&SM::Sigeq),
-  "Equivalent stress deviator",
-  py::arg("Sig"));
+sm.def("I4d", &SM::I4d, "Fourth order deviatoric projection tensor.");
 
-// -------------------------------------------------------------------------------------------------
+// Tensor algebra
 
 sm.def("Hydrostatic",
-  py::overload_cast<const xt::xtensor<double,3>&>(&SM::Hydrostatic),
-  "Hydrostatic part of a 2nd-order tensor",
-  py::arg("A"));
-
-sm.def("Deviatoric",
-  py::overload_cast<const xt::xtensor<double,3>&>(&SM::Deviatoric),
-  "Deviatoric",
-  py::arg("A"));
-
-sm.def("Epseq",
-  py::overload_cast<const xt::xtensor<double,3>&>(&SM::Epseq),
-  "Equivalent strain deviator",
-  py::arg("Eps"));
-
-sm.def("Sigeq",
-  py::overload_cast<const xt::xtensor<double,3>&>(&SM::Sigeq),
-  "Equivalent stress deviator",
-  py::arg("Sig"));
-
-// -------------------------------------------------------------------------------------------------
+    py::overload_cast<const SM::Tensor2&>(&SM::Hydrostatic),
+    "Hydrostatic part of a 2nd-order tensor. Returns scalar.",
+    py::arg("A"));
 
 sm.def("Hydrostatic",
-  py::overload_cast<const xt::xtensor<double,4>&>(&SM::Hydrostatic),
-  "Hydrostatic part of a 2nd-order tensor",
-  py::arg("A"));
+    py::overload_cast<const xt::xtensor<double,3>&>(&SM::Hydrostatic),
+    "Hydrostatic part of a 2nd-order tensor. Returns list of scalars.",
+    py::arg("A"));
+
+sm.def("Hydrostatic",
+    py::overload_cast<const xt::xtensor<double,4>&>(&SM::Hydrostatic),
+    "Hydrostatic part of a 2nd-order tensor. Returns matrix of scalars.",
+    py::arg("A"));
 
 sm.def("Deviatoric",
-  py::overload_cast<const xt::xtensor<double,4>&>(&SM::Deviatoric),
-  "Deviatoric",
-  py::arg("A"));
+    py::overload_cast<const SM::Tensor2&>(&SM::Deviatoric),
+    "Deviatoric part of a 2nd-order tensor. Returns 2nd-order tensor.",
+    py::arg("A"));
+
+sm.def("Deviatoric",
+    py::overload_cast<const xt::xtensor<double,3>&>(&SM::Deviatoric),
+    "Deviatoric part of a 2nd-order tensor. Returns list 2nd-order tensors.",
+    py::arg("A"));
+
+sm.def("Deviatoric",
+    py::overload_cast<const xt::xtensor<double,4>&>(&SM::Deviatoric),
+    "Deviatoric part of a 2nd-order tensor. Returns matrix 2nd-order tensors.",
+    py::arg("A"));
 
 sm.def("Epseq",
-  py::overload_cast<const xt::xtensor<double,4>&>(&SM::Epseq),
-  "Equivalent strain deviator",
-  py::arg("Eps"));
+    py::overload_cast<const SM::Tensor2&>(&SM::Epseq),
+    "Equivalent strain deviator. Returns scalar.",
+    py::arg("Eps"));
+
+sm.def("Epseq",
+    py::overload_cast<const xt::xtensor<double,3>&>(&SM::Epseq),
+    "Equivalent strain deviator. Returns list of scalars.",
+    py::arg("Eps"));
+
+sm.def("Epseq",
+    py::overload_cast<const xt::xtensor<double,4>&>(&SM::Epseq),
+    "Equivalent strain deviator. Returns matrix of scalars.",
+    py::arg("Eps"));
 
 sm.def("Sigeq",
-  py::overload_cast<const xt::xtensor<double,4>&>(&SM::Sigeq),
-  "Equivalent stress deviator",
-  py::arg("Sig"));
+    py::overload_cast<const SM::Tensor2&>(&SM::Sigeq),
+    "Equivalent stress deviator. Returns scalar.",
+    py::arg("Sig"));
 
-// -------------------------------------------------------------------------------------------------
+sm.def("Sigeq",
+    py::overload_cast<const xt::xtensor<double,3>&>(&SM::Sigeq),
+    "Equivalent stress deviator. Returns list of scalars.",
+    py::arg("Sig"));
+
+sm.def("Sigeq",
+    py::overload_cast<const xt::xtensor<double,4>&>(&SM::Sigeq),
+    "Equivalent stress deviator. Returns matrix of scalars.",
+    py::arg("Sig"));
+
+// Material point: Elastic
 
 py::class_<SM::Elastic>(sm, "Elastic")
 
-  .def(
-    py::init<double, double>(),
-    "Linear elastic material point",
-    py::arg("K"),
-    py::arg("G")
-  )
+    .def(py::init<double, double>(), "Linear elastic material point", py::arg("K"), py::arg("G"))
 
-  .def("K", &SM::Elastic::K)
-  .def("G", &SM::Elastic::G)
-  .def("Stress", &SM::Elastic::Stress, py::arg("Eps"))
-  .def("Tangent", &SM::Elastic::Tangent, py::arg("Eps"))
+    .def("K", &SM::Elastic::K, "Returns the bulk modulus")
 
-  .def("__repr__", [](const SM::Elastic &){
-    return "<GMatElastic.Cartesian3d.Elastic>"; });
+    .def("G", &SM::Elastic::G, "Returns the shear modulus")
 
-// -------------------------------------------------------------------------------------------------
+    .def("Stress",
+        &SM::Elastic::Stress,
+        "Returns stress tensor, for a given strain tensor.",
+        py::arg("Eps"))
+
+    .def("Tangent",
+        &SM::Elastic::Tangent,
+        "Returns tangent stiffness tensor (fourth order), for a given strain tensor.",
+        py::arg("Eps"))
+
+    .def("__repr__", [](const SM::Elastic&) { return "<GMatElastic.Cartesian3d.Elastic>"; });
+
+// Material identifier
 
 py::module smm = sm.def_submodule("Type", "Type enumerator");
 
@@ -132,54 +135,75 @@ py::enum_<SM::Type::Value>(smm, "Type")
     .value("Elastic", SM::Type::Elastic)
     .export_values();
 
-// -------------------------------------------------------------------------------------------------
+// Matrix
 
 py::class_<SM::Matrix>(sm, "Matrix")
 
-  .def(
-    py::init<size_t, size_t>(),
-    "Matrix of linear elastic material points",
-    py::arg("nelem"),
-    py::arg("nip")
-  )
+    .def(py::init<size_t, size_t>(),
+        "Matrix of material points",
+        py::arg("nelem"),
+        py::arg("nip"))
 
-  .def(
-    py::init<size_t, size_t, double, double>(),
-    "Matrix of linear elastic material points",
-    py::arg("nelem"),
-    py::arg("nip"),
-    py::arg("K"),
-    py::arg("G"))
+    .def(py::init<size_t, size_t, double, double>(),
+        "Matrix of linear elastic material points",
+        py::arg("nelem"),
+        py::arg("nip"),
+        py::arg("K"),
+        py::arg("G"))
 
-  .def("ndim", &SM::Matrix::ndim)
-  .def("nelem", &SM::Matrix::nelem)
-  .def("nip", &SM::Matrix::nip)
+    .def("ndim", &SM::Matrix::ndim, "Return number of (tensor) dimensions.")
 
-  .def("K", &SM::Matrix::K)
-  .def("G", &SM::Matrix::G)
+    .def("nelem", &SM::Matrix::nelem, "Return number of elements (matrix rows).")
 
-  .def("I2", &SM::Matrix::I2)
-  .def("II", &SM::Matrix::II)
-  .def("I4", &SM::Matrix::I4)
-  .def("I4rt", &SM::Matrix::I4rt)
-  .def("I4s", &SM::Matrix::I4s)
-  .def("I4d", &SM::Matrix::I4d)
+    .def("nip", &SM::Matrix::nip, "Return number of integration points (matrix columns).")
 
-  .def("check", &SM::Matrix::check)
+    .def("K", &SM::Matrix::K, "Return matrix with bulk moduli.")
 
-  .def("setElastic",
-    &SM::Matrix::setElastic,
-    py::arg("I"),
-    py::arg("K"),
-    py::arg("G"))
+    .def("G", &SM::Matrix::G, "Return matrix with shear moduli.")
 
-  .def("Stress", &SM::Matrix::Stress, py::arg("Eps"))
-  .def("Tangent", &SM::Matrix::Tangent, py::arg("Eps"))
+    .def("I2", &SM::Matrix::I2, "Return matrix with second order unit tensors.")
 
-  .def("__repr__", [](const SM::Matrix &){
-    return "<GMatElastic.Cartesian3d.Matrix>"; });
+    .def("II",
+        &SM::Matrix::II,
+        "Return matrix with fourth order tensors with the result of the dyadic product II.")
 
-// -------------------------------------------------------------------------------------------------
+    .def("I4", &SM::Matrix::I4, "Return matrix with fourth order unit tensors.")
+
+    .def("I4rt",
+        &SM::Matrix::I4rt,
+        "Return matrix with fourth right-transposed order unit tensors.")
+
+    .def("I4s",
+        &SM::Matrix::I4s,
+        "Return matrix with fourth order symmetric projection tensors.")
+
+    .def("I4d",
+        &SM::Matrix::I4d,
+        "Return matrix with fourth order deviatoric projection tensors.")
+
+    .def("type", &SM::Matrix::type, "Return matrix with material types.")
+
+    .def("check",
+        &SM::Matrix::check,
+        "Check that all matrix entries are set. Throws if any unset point is found.")
+
+    .def("setElastic",
+        &SM::Matrix::setElastic,
+        "Set specific entries 'Elastic'.",
+        py::arg("I"),
+        py::arg("K"),
+        py::arg("G"))
+
+    .def("Stress",
+        &SM::Matrix::Stress,
+        "Returns matrix of stress tensors, for a given matrix of strain tensors.",
+        py::arg("Eps"))
+
+    .def("Tangent",
+        &SM::Matrix::Tangent,
+        "Returns matrix of tangent stiffness tensors, for a given matrix of strain tensors.",
+        py::arg("Eps"))
+
+    .def("__repr__", [](const SM::Matrix&) { return "<GMatElastic.Cartesian3d.Matrix>"; });
 
 }
-
