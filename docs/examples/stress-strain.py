2f9654e25d4853f143b4da96299e8491599e63a7
Tom de Geus <tom@geus.me>
2020-02-12 08:42:40 +0100

    Updating readme

--- docs/examples/stress-strain.py
+++ docs/examples/stress-strain.py
@@ -1,34 +1,42 @@
-import GMatElastic       as gmat
+import GMatElastic as gmat
 import matplotlib.pyplot as plt
-import numpy             as np
+import numpy as np
 
-plt.style.use(['goose', 'goose-latex'])
+try:
+    plt.style.use(['goose', 'goose-latex'])
+except:
+    pass
 
-ddot42 = lambda A4,B2: np.einsum('ijkl,lk->ij',A4,B2)
-ddot22 = lambda A2,B2: np.einsum('ij,ji',A2,B2)
+def ddot42(A4, B2):
+    return np.einsum('ijkl,lk->ij', A4, B2)
+
+def ddot22(A2, B2):
+    return np.einsum('ij,ji', A2, B2)
 
 I4d = gmat.Cartesian3d.I4d()
 
-mat = gmat.Cartesian3d.Elastic(10., 1.)
+mat = gmat.Cartesian3d.Elastic(10.0, 1.0)
 
 epseq = np.zeros(101)
 sigeq = np.zeros(101)
 
 for igamma, gamma in enumerate(np.linspace(0.0, 0.1, len(epseq))):
 
-  Eps = np.array([
-    [  0.0, gamma,   0.0],
-    [gamma,   0.0,   0.0],
-    [  0.0,   0.0,   0.0],
-  ])
+    Eps = np.array([
+        [0.0, gamma, 0.0],
+        [gamma, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+    ])
 
-  Sig = mat.Stress(Eps)
+    Sig = mat.Stress(Eps)
 
-  Epsd = ddot42(I4d,Eps)
-  Sigd = ddot42(I4d,Sig)
+    Epsd = ddot42(I4d, Eps)
+    Sigd = ddot42(I4d, Sig)
 
-  epseq[igamma] = np.sqrt(2./3.*ddot22(Epsd,Epsd))
-  sigeq[igamma] = np.sqrt(3./2.*ddot22(Sigd,Sigd))
+    epseq[igamma] = np.sqrt(2.0 / 3.0 * ddot22(Epsd, Epsd))
+    sigeq[igamma] = np.sqrt(3.0 / 2.0 * ddot22(Sigd, Sigd))
+
+# Plot
 
 fig, ax = plt.subplots()
 
