eb486aab7a63d98e06cb2aa793b458f57689a1f5
Tom de Geus <tdegeus@users.noreply.github.com>
2021-08-30 07:10:00 +0200

    Using scikit-build (#21)

---
+++ include/GMatElastic/version.hpp
@@ -0,0 +1,46 @@
+/**
+\file
+\copyright Copyright 2018. Tom de Geus. All rights reserved.
+\license This project is released under the MIT License.
+*/
+
+#ifndef GMATELASTIC_VERSION_HPP
+#define GMATELASTIC_VERSION_HPP
+
+#include "version.h"
+
+namespace GMatElastic {
+
+namespace detail {
+
+    inline std::string unquote(const std::string& arg)
+    {
+        std::string ret = arg;
+        ret.erase(std::remove(ret.begin(), ret.end(), '\"'), ret.end());
+        return ret;
+    }
+
+}
+
+inline std::string version()
+{
+    return detail::unquote(std::string(QUOTE(GMATELASTIC_VERSION)));
+}
+
+inline std::vector<std::string> version_dependencies()
+{
+    std::vector<std::string> ret;
+
+    ret.push_back("gmatelastic=" + version());
+    ret.push_back("gmattensor=" + GMatTensor::version());
+    ret.push_back("xtensor=" +
+        detail::unquote(std::string(QUOTE(XTENSOR_VERSION_MAJOR))) + "." +
+        detail::unquote(std::string(QUOTE(XTENSOR_VERSION_MINOR))) + "." +
+        detail::unquote(std::string(QUOTE(XTENSOR_VERSION_PATCH))));
+
+    return ret;
+}
+
+} // namespace GMatElastic
+
+#endif
--- setup.py
+++ setup.py
@@ -1,54 +1,19 @@
-desc = '''
-Linear elastic material model.
-'''
+from skbuild import setup
+from setuptools_scm import get_version
 
-from setuptools import setup, Extension
-
-import re
-import os
-import pybind11
-import pyxtensor
-
-header = open('include/GMatElastic/config.h', 'r').read()
-major = re.split(r'(.*)(\#define GMATELASTIC_VERSION_MAJOR\ )([0-9]+)(.*)', header)[3]
-minor = re.split(r'(.*)(\#define GMATELASTIC_VERSION_MINOR\ )([0-9]+)(.*)', header)[3]
-patch = re.split(r'(.*)(\#define GMATELASTIC_VERSION_PATCH\ )([0-9]+)(.*)', header)[3]
-
-__version__ = '.'.join([major, minor, patch])
-
-include_dirs = [
-    os.path.abspath('include/'),
-    pyxtensor.find_pyxtensor(),
-    pyxtensor.find_pybind11(),
-    pyxtensor.find_xtensor(),
-    pyxtensor.find_xtl()]
-
-build = pyxtensor.BuildExt
-
-xsimd = pyxtensor.find_xsimd()
-if xsimd:
-    if len(xsimd) > 0:
-        include_dirs += [xsimd]
-        build.c_opts['unix'] += ['-march=native', '-DXTENSOR_USE_XSIMD']
-        build.c_opts['msvc'] += ['/DXTENSOR_USE_XSIMD']
-
-ext_modules = [Extension(
-    'GMatElastic',
-    ['python/main.cpp'],
-    include_dirs = include_dirs,
-    language = 'c++')]
+project_name = "GMatElastic"
 
 setup(
-    name = 'GMatElastic',
-    description = 'Linear elastic material model.',
-    long_description = desc,
-    keywords = 'Material model; FEM; FFT',
-    version = __version__,
-    license = 'MIT',
-    author = 'Tom de Geus',
-    author_email = 'tom@geus.me',
-    url = 'https://github.com/tdegeus/GMatElastic',
-    ext_modules = ext_modules,
-    install_requires = ['pybind11', 'pyxtensor'],
-    cmdclass = {'build_ext': build},
-    zip_safe = False)
+    name = project_name,
+    description = "Linear elastic material model.",
+    long_description = "Linear elastic material model.",
+    version = get_version(),
+    license = "MIT",
+    author = "Tom de Geus",
+    author_email = "tom@geus.me",
+    url = f"https://github.com/tdegeus/{project_name}",
+    packages = [f"{project_name}"],
+    package_dir = {"": "python"},
+    cmake_install_dir = f"python/{project_name}",
+    cmake_minimum_required_version = "3.13...3.21",
+)
