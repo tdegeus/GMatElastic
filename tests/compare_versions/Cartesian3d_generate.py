c877fe217782ea700c5fe09b26077fa93a39f655
Tom de Geus <tom@geus.me>
2022-08-19 07:59:52 +0200

    [tests] Simplifying historic tests

--- tests/compare_versions/Cartesian3d_generate.py
+++ tests/compare_versions/Cartesian3d_generate.py
@@ -6,20 +6,19 @@ import numpy as np
 with h5py.File("Cartesian3d_random.h5", "w") as data:
 
     shape = [1000, 4]
-    K = 0.5 + np.random.random(shape)
-    G = 0.5 + np.random.random(shape)
 
-    data["/model/K"] = K
-    data["/model/G"] = G
+    mat = GMat.Elastic2d(
+        K=0.5 + np.random.random(shape),
+        G=0.5 + np.random.random(shape),
+    )
 
-    mat = GMat.Elastic2d(K, G)
-    I4s = tensor.Array2d(mat.shape).I4s
+    data["K"] = mat.K
+    data["G"] = mat.G
 
     for i in range(20):
 
-        GradU = 200 * np.random.random(shape + [3, 3])
-        data[f"/random/{i:d}/GradU"] = GradU
-        mat.Eps = tensor.A4_ddot_B2(I4s, GradU)
+        mat.Eps = tensor.Sym(20 * np.random.random(shape + [3, 3]))
 
-        data[f"/random/{i:d}/Stress"] = mat.Sig
-        data[f"/random/{i:d}/Tangent"] = mat.C
+        data[f"/data/{i:d}/Eps"] = mat.Eps
+        data[f"/data/{i:d}/Stress"] = mat.Sig
+        data[f"/data/{i:d}/Tangent"] = mat.C
